5f806464e5b3bc75c4874860b697265634833526
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2024-08-19 21:23:07 +0300

    Create a helper function to get user DMs sorted from newest to oldest

--- src/helpers/apiHandler.js
+++ src/helpers/apiHandler.js
@@ -46,3 +46,40 @@ function getUserAvatar(userId, avatarId) {
     const avatarUrl = `https://cdn.discordapp.com/avatars/${userId}/${avatarId}.png`
     return avatarId ? avatarUrl : "";
 }
+
+async function getUserDms() {
+const url = `${API_URL}users/@me/channels`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                "Authorization": `${TOKEN}`
+            }
+        });
+
+        const channels = response.data
+            .filter(channel => channel.type === 1)
+            .map(async channel => {
+                const user = channel.recipients[0];
+                const userAvatar = getUserAvatar(user.id, user.avatar);
+                const lastMessageTimestamp = await getLastMessageTimestamp(channel.id);
+
+                return {
+                    id: channel.id,
+                    lastMessageTimestamp,
+                    user: {
+                        id: user.id,
+                        username: user.username,
+                        globalName: user.global_name,
+                        avatarUrl: userAvatar
+                    }
+                }
+            });
+
+        const sortedChannels = (await Promise.all(channels)).sort((a, b) => b.lastMessageTimestamp - a.lastMessageTimestamp);
+
+        return JSON.stringify(sortedChannels, null, 2);
+    } catch (err) {
+        console.error('Error in DM listing: ' + err)
+    }
+}
