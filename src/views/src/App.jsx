b3be6bbe0d852d3d099c10630adf3bb0cfce24f2
burakbehlull <burakyabgu@gmail.com>
2024-09-11 11:42:33 +0300

    feat: Create scss style file

--- src/views/src/App.jsx
+++ src/views/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import Router from "./routes/router"
 
-
+import './main.scss'
 
 function App() {
   const navigate = useNavigate()
