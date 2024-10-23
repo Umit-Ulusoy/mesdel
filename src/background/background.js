0c7bffc548573c79d3554f79576705bd8a87bbb7
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2024-10-23 21:05:39 +0300

    feat: Add a service worker logic to handle message deletion process

--- src/background/background.js
+++ src/background/background.js
@@ -1,7 +1,80 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'SEND_TOKEN') {
-        chrome.storage.local.set({ token: message.token }, () => {
-            console.log('Token stored');
-        })
+import { delay } from '../helpers/misc.js';
+import { getUserId } from '../helpers/apiHandler.js';
+import { getUserToken } from '../helpers/tokenHandler.js';
+import { setIconBusy, setIconIdle, setIconSuccess} from '../helpers/iconHandler.js';
+import { showNotification } from '../helpers/notificationHandler.js';
+
+let
+authorId,
+progress = 0,
+isPopupOpen = false,
+isProcessing = false,
+deletedMessageCount = 0,
+totalMessageCountToDelete,
+lastMessageId,
+TOKEN;
+
+const API_URL = 'https://discord.com/api/v9';
+
+chrome.runtime.onConnect.addListener((port) => {
+
+    if (port.name !== 'MAIN_POPUP') return;
+
+    isPopupOpen = true;
+  
+    port.onDisconnect.addListener(() => {
+        isPopupOpen = false;
+    });
+
+    return;
+  });
+
+  
+chrome.runtime.onMessage.addListener( async (request, sender, sendResponse) => {
+
+    if (request.action !== 'START_MESSAGE_DELETION' || isProcessing) return;
+
+    if (!TOKEN) {
+        TOKEN = (await getUserToken())
+        .replace(/"/g, '');
     }
-})
+
+    authorId = await getUserId(TOKEN);
+
+    setIconBusy();
+    isProcessing = true;
+    
+    const {
+        channelId,
+        messageCount
+    } = request;
+
+    let restMessageCount = messageCount;
+    totalMessageCountToDelete = messageCount;
+
+    while (restMessageCount > 0) {
+        const messageCountToDeleteNow = (restMessageCount >= 20) ? 20 : restMessageCount;
+        
+        await deleteMessages(channelId, authorId, messageCountToDeleteNow);
+        restMessageCount -= messageCountToDeleteNow;
+    }
+
+    setIconSuccess();
+
+    isProcessing = false;
+    progress = 0;
+    deletedMessageCount = 0;
+    lastMessageId = null;
+
+    if (isPopupOpen) {
+        await chrome.runtime.sendMessage({
+            action: 'MESSAGE_DELETION_COMPLETE'
+        });
+    } else {
+    await showNotification(
+        'MesDel',
+         `Mesaj silme işlemi tamamlandı! ${totalMessageCountToDelete} mesaj başarıyla uzaya fırlatıldı!`,
+        'action_success.png');
+    }
+});
+
