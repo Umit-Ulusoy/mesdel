854f6695041e734cc3a9cc09b87949e772b12e8d
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2024-10-23 13:31:45 +0300

    fix: Fix path accessing issue  for icon-helper functions
    Icon paths has been fetched successfully with chrome.runtime.getURL() function \n\nicon-helper functions have been renamed from 'changeIconTo…' to 'setIcon…' for improved clarity

--- src/helpers/iconHandler.js
+++ src/helpers/iconHandler.js
@@ -1,14 +1,23 @@
 // Set the icon to "busy" to indicate that a task is in progress.
-function changeIconToBusy() {
-    chrome.action.setIcon({ path: 'src/icons/icon_busy.png' });
+function setIconBusy() {
+    const path = chrome.runtime.getURL('src/icons/icon_busy.png');
+    chrome.action.setIcon({ path });
 }
 
 // Set the icon to "idle" to indicate that nothing is in progress.
-function changeIconToIdle() {
-    chrome.action.setIcon({ path: 'src/icons/icon_idle.png' });
+function setIconIdle() {
+    const path = chrome.runtime.getURL('src/icons/icon_idle.png');
+    chrome.action.setIcon({ path });
 }
 
-// Set the icon to "completed" to indicate that the task is finished.
-function changeIconToCompleted() {
-    chrome.action.setIcon({ path: 'src/icons/icon_completed.png' });
+// Set the icon to "success" to indicate that the task is successfully completed.
+function setIconSuccess() {
+    const path = chrome.runtime.getURL('src/icons/icon_success.png');
+    chrome.action.setIcon({ path });
+}
+
+export {
+    setIconBusy,
+    setIconIdle,
+    setIconSuccess
 }
